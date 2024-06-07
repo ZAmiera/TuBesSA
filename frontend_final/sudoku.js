835868b621b70349631e3423077b39f91a78d8a2
Zahra Amiera Putri Sailendra <101447721+ZAmiera@users.noreply.github.com>
2024-06-07 17:38:44 +0700

    upload frontend_final

---
+++ frontend_final/sudoku.js
@@ -0,0 +1,79 @@
+var board = [
+    "316578492",
+    "529134768",
+    "487629531",
+    "263415987",
+    "974863125",
+    "851792643",
+    "138947256",
+    "692351874",
+    "745286319"
+]
+
+var solution = [
+    "316578492",
+    "529134768",
+    "487629531",
+    "263415987",
+    "974863125",
+    "851792643",
+    "138947256",
+    "692351874",
+    "745286319"
+]
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // Board 9x9
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+
+        // "0-0" "0-1" .. "3-1"
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+    }
+}
